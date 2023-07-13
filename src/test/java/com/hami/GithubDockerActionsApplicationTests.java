01567e2847ab3219380ff8106d36e0759cfacc77
hamed zarrabi <47312982+hamedzarrabi@users.noreply.github.com>
2023-07-13 11:33:16 +0330

    first commit

---
+++ src/test/java/com/hami/GithubDockerActionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hami;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubDockerActionsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
